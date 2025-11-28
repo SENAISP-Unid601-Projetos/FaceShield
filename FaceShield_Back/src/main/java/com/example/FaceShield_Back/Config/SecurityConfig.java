aa92da3ada53777680cbd68dc38852b15f6dc98b
Alexandre Santos <alexandre.santana2201@gmail.com>
2025-11-28 15:33:36 -0300

    Att de config do security para utilizar o CORS certo

--- FaceShield_Back/src/main/java/com/example/FaceShield_Back/Config/SecurityConfig.java
+++ FaceShield_Back/src/main/java/com/example/FaceShield_Back/Config/SecurityConfig.java
@@ -11,7 +11,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy; // Importação correta
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -24,7 +24,6 @@ public class SecurityConfig {
     @Autowired
     private CustomUserDetailsService userDetailsService;
 
-    // 2. Injete o seu SecurityFilter
     @Autowired
     private SecurityFilter securityFilter;
 
@@ -32,7 +31,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
-                .cors(Customizer.withDefaults())
+                .cors(cors -> {}) // Deixa o WebMvcConfigurer cuidar do CORS
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
                         // LIBERA OS ENDPOINTS DE HEALTH CHECK
